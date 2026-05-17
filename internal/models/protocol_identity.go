039814437c11fe6359b3b581c3eff89a808b28b6
Akif Gultekin <gultekinmakif@gmail.com>
2026-05-17 10:15:54 +0200

    fix(db): pluralize table name

--- internal/models/protocol_identity.go
+++ internal/models/protocol_identity.go
@@ -6,9 +6,9 @@ import "github.com/lib/pq"
 type ProtocolIdentity struct {
 	Slug     string         `gorm:"column:slug;type:text;not null;primaryKey" json:"slug"`
 	Name     string         `gorm:"column:name;type:text;not null" json:"name"`
-	Category *string        `gorm:"column:category;type:text;index:idx_protocol_identity_category" json:"category,omitempty"`
-	Chains   pq.StringArray `gorm:"column:chains;type:text[];not null;default:'{}';index:idx_protocol_identity_chains,type:gin" json:"chains"`
+	Category *string        `gorm:"column:category;type:text;index:idx_protocol_identities_category" json:"category,omitempty"`
+	Chains   pq.StringArray `gorm:"column:chains;type:text[];not null;default:'{}';index:idx_protocol_identities_chains,type:gin" json:"chains"`
 	DataFile *string        `gorm:"column:data_file;type:text" json:"data_file,omitempty"`
 }
 
-func (ProtocolIdentity) TableName() string { return "protocol_identity" }
+func (ProtocolIdentity) TableName() string { return "protocol_identities" }
